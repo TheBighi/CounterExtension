f65cf157af2ce8c1772220cc3295eb4b8d7c090a
TheBighi <thebighix@gmail.com>
2025-09-28 01:40:09 +0300

    Made it look better, also added a link to the manga page

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const DecreaseBTN = document.getElementById("DecreaseBTN")
 const IncreaseBTN = document.getElementById("IncreaseBTN")
 const CountOutput = document.getElementById("Count")
+const MangaPage = document.getElementById("LinkToManga")
+
+const CSMLink = "https://chainsawmann.com/manga/chainsaw-man-chapter-"
 
 chrome.storage.local.get(["count"], (count) => {
     let currentCount = count.count ?? 0;
     CountOutput.textContent = currentCount;
+    MangaPage.setAttribute("href",CSMLink + currentCount)
 
     DecreaseBTN.onclick = function(){
         currentCount--;
         CountOutput.textContent = currentCount;
         chrome.storage.local.set({count: currentCount});
         console.log("Increase")
+        MangaPage.setAttribute("href",CSMLink + currentCount)
     }
     IncreaseBTN.onclick = function(){
         currentCount++;
         CountOutput.textContent = currentCount;
         chrome.storage.local.set({count: currentCount});
         console.log("Increase")
+        MangaPage.setAttribute("href",CSMLink + currentCount)
     }
 })
+
+//document.getElementById("menu-item-7062").setAttribute("href", "my new link");
