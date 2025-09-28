97763e868c45eefdc9be8a2ae5d2e359a2e82d4b
TheBighi <thebighix@gmail.com>
2025-09-28 19:12:00 +0300

    Added functionality of when going onto the next chapter it automatically updates, this only happens when the chapter u go on is only 1 higher than the current count, also made it cleaner

--- index.js
+++ index.js
@@ -5,23 +5,48 @@ const MangaPage = document.getElementById("LinkToManga")
 
 const CSMLink = "https://chainsawmann.com/manga/chainsaw-man-chapter-"
 
-chrome.storage.local.get(["count"], (count) => {
-    let currentCount = count.count ?? 0;
-    CountOutput.textContent = currentCount;
-    MangaPage.setAttribute("href",CSMLink + currentCount)
+// init
+chrome.storage.local.get(["count"], (result) => {
+    currentCount = result.count ?? 0;
+    UpdateUI(currentCount);
+});
 
-    DecreaseBTN.onclick = function(){
-        currentCount--;
-        CountOutput.textContent = currentCount;
-        chrome.storage.local.set({count: currentCount});
-        console.log("Increase")
-        MangaPage.setAttribute("href",CSMLink + currentCount)
+function SetCount(NewCount){
+    currentCount = NewCount;
+    chrome.storage.local.set({ count: currentCount });
+}
+
+function UpdateUI(NewCount){
+    CountOutput.textContent = "Chapter: " + NewCount;
+    MangaPage.setAttribute("href",CSMLink + NewCount)
+}
+
+DecreaseBTN.onclick = function() {
+    SetCount(currentCount - 1)
+    console.log("Decrease");
+    UpdateUI(currentCount);
+}
+
+IncreaseBTN.onclick = function() {
+    SetCount(currentCount + 1)
+    console.log("Increase");
+    UpdateUI(currentCount);
+}
+
+function GetCurrentURL() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      resolve(tabs[0].url);
+    });
+  });
+}
+async function CheckURL(){
+    const url = await GetCurrentURL()
+    const match = url.match(/chapter-(\d+)/);
+    const chapter = parseInt(match[1], 10);
+    if (currentCount < chapter < currentCount + 2){
+        SetCount(chapter)
+        UpdateUI(chapter)
     }
-    IncreaseBTN.onclick = function(){
-        currentCount++;
-        CountOutput.textContent = currentCount;
-        chrome.storage.local.set({count: currentCount});
-        console.log("Increase")
-        MangaPage.setAttribute("href",CSMLink + currentCount)
-    }
-})
+}
+CheckURL()
